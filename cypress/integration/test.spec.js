0573758c355d0be51085b60052e9fd8513a93c5a
sebb94 <omgl0lwth@gmail.com>
2020-09-20 16:07:46 +0200

    verify api call

--- cypress/integration/test.spec.js
+++ cypress/integration/test.spec.js
@@ -5,7 +5,24 @@ describe('First test', () => {
     });
     
     it('first step', () => {
-      
+
+      cy.server()
+      cy.route('POST','**/articles').as('postArticles')
+
+
+      cy.get('a[href="/editor"').click()
+      cy.get('[formcontrolname="title"]').type("Article title")
+      cy.get('[formcontrolname="description"]').type("Article description")
+      cy.get('[formcontrolname="body"]').type("Article body")
+      cy.get('[type="button"]').click()
+
+      cy.wait('@postArticles')
+      cy.get('@postArticles').then(xhr =>{
+          cy.log(xhr)
+          expect(xhr.status).to.equal(200)
+          expect(xhr.request.body.article.body).to.equal('Article body')
+          expect(xhr.response.body.article.description).to.equal('Article description')
+      })
     });
 
  
